0445f9de2ab6a6e54d03cf34024bd19c60304872
Abhishek <M1050754@mindtree.com>
2020-08-20 01:34:34 +0530

    added filter and text-to-speech

--- build/precache-manifest.dbab795d072fb59e5f6cbfa85cfc0b7d.js
+++ build/precache-manifest.dbab795d072fb59e5f6cbfa85cfc0b7d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "55e2e6b15779f67c06e1c43015a008b6",
+    "revision": "c127e1199a926629b05af29745ff91ce",
     "url": "/index.html"
   },
   {
-    "revision": "fd9860c72df9e9c80b97",
-    "url": "/static/js/2.5c52ae99.chunk.js"
+    "revision": "93314df2a5fe080f1cb5",
+    "url": "/static/js/2.0597f83e.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.5c52ae99.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.0597f83e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eec22fbd58a01cda588f",
-    "url": "/static/js/main.0af62fc8.chunk.js"
+    "revision": "6574da5b817ddd561e5e",
+    "url": "/static/js/main.08eb2d12.chunk.js"
   },
   {
     "revision": "559ff28579f192bdf8b9",
