0132dd7c34b73e198ea6790287493f6d070c4e95
Abhishek <M1050754@mindtree.com>
2020-08-19 18:46:45 +0530

    sorting fix added

---
+++ build/precache-manifest.92cd5738f954968fb9373c8b740fd7dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2febdc6ff578f8bfdb1dd24b0b224155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd9860c72df9e9c80b97",
+    "url": "/static/js/2.5c52ae99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c52ae99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5835ec56c0dc742dbc27",
+    "url": "/static/js/main.41d56d7a.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
